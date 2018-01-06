09ac4b8878196a3adec0e7532c3c47848f3f7dbc
Kirill Buzan <kir-andy@mail.ru>
2018-01-07 02:41:03 +0700

    Homework #08. Terraform-01

---
+++ terraform/variables.tf
@@ -0,0 +1,34 @@
+variable project {
+  description = "Project ID"
+}
+
+variable region {
+  description = "Region"
+  default     = "europe-west1"
+}
+
+variable public_key_path {
+  description = "Path to the public key used for ssh access"
+}
+
+variable disk_image {
+  description = "Disk image"
+}
+
+variable private_key_path {
+  description = "Path to the private key used for ssh access"
+}
+
+variable zone {
+  description = "Zone"
+  default     = "europe-west1-b"
+}
+
+variable users {
+  description = "Users list"
+}
+
+variable count_devices {
+  description = "Count created instances"
+  default     = "1"
+}
