1cc19cbf0db3af55aa3931999483b81a4e25574c
Kirill Buzan <kir-andy@mail.ru>
2018-01-14 10:14:50 +0700

    Homework #09

---
+++ terraform/modules/vpc/main.tf
@@ -0,0 +1,10 @@
+resource "google_compute_firewall" "firewall_ssh" {
+  name = "default-allow-ssh"
+  description = "Allow SSH from anywhere.HM#9"
+  network = "default"
+  allow {
+    protocol = "tcp"
+    ports = ["22"]
+  }
+  source_ranges = "${var.source_ranges}"
+}
